a08a3a40240e87098748c0d75bd8d8afc5f1fb8b
KingTingTheGreat <jting@bu.edu>
2023-04-05 00:19:57 -0400

    moved is_valid_state function to twisty_cube.rs file from main.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,7 @@
+mod twisty_cube;
 use std::io::stdin;
 use std::collections::{HashMap, HashSet};
 
-/*
-returns boolean indicating whether the state is valid
-this means it is a string of length 54 and contains 9 of each color
- */
-fn is_valid_state(state: String) -> bool {
-    // for a 3x3x3 cube
-    let total_num_colors = 54;
-    let quantity_per_color = 9;
-    if state.len() != total_num_colors {
-        println!("Invalid state length: {}", state.len());
-        return false;
-    }
-    // set of valid colors
-    let valid_colors:HashSet<char>= HashSet::from_iter(['W', 'O', 'G', 'R', 'B', 'Y'].iter().cloned());
-    // check that there are 9 of each color
-    let mut colors:HashMap<char, i32> = HashMap::new();
-    // add quantity of each color to hashmap
-    for color in state.chars() {
-        // println!("char: {}", color);  // printing each input character
-        if colors.contains_key(&color) {
-            colors.insert(color, colors[&color] + 1);    
-        }
-        else {
-            colors.insert(color, 1);
-        }
-    }
-    // check that each color is valid and has quantity of 9
-    for color in colors.keys() {
-        if !valid_colors.contains(&color) {
-            println!("Invalid color: {}", color);
-            return false;
-        }
-        if colors[color] != quantity_per_color {
-            println!("Invalid quantity of color {}: {}", color, colors[color]);
-            return false;
-        }
-    }
-    return true;
-}
 
 fn main() {
     let mut user_input = String::new();
@@ -48,7 +10,7 @@ fn main() {
     // remove whitespace characters
     let initial_state = user_input.trim().to_string();
     // check if state is valid
-    if !is_valid_state(initial_state.clone()) {
+    if !twisty_cube::is_valid_state(initial_state.clone()) {
         println!("Invalid state");
         return;
     }
--- src/twisty_cube.rs
+++ src/twisty_cube.rs
@@ -1,11 +1,51 @@
-use std::ptr::null;
-use main::is_valid_state;
+// use std::ptr::null;
+use std::collections::{HashMap, HashSet};
+
+/*
+returns boolean indicating whether the state is valid
+this means it is a string of length 54 and contains 9 of each color
+ */
+pub fn is_valid_state(state: String) -> bool {
+    // for a 3x3x3 cube
+    let total_num_colors = 54;
+    let quantity_per_color = 9;
+    if state.len() != total_num_colors {
+        println!("Invalid state length: {}", state.len());
+        return false;
+    }
+    // set of valid colors
+    let valid_colors:HashSet<char>= HashSet::from_iter(['W', 'O', 'G', 'R', 'B', 'Y'].iter().cloned());
+    // check that there are 9 of each color
+    let mut colors:HashMap<char, i32> = HashMap::new();
+    // add quantity of each color to hashmap
+    for color in state.chars() {
+        // println!("char: {}", color);  // printing each input character
+        if colors.contains_key(&color) {
+            colors.insert(color, colors[&color] + 1);    
+        }
+        else {
+            colors.insert(color, 1);
+        }
+    }
+    // check that each color is valid and has quantity of 9
+    for color in colors.keys() {
+        if !valid_colors.contains(&color) {
+            println!("Invalid color: {}", color);
+            return false;
+        }
+        if colors[color] != quantity_per_color {
+            println!("Invalid quantity of color {}: {}", color, colors[color]);
+            return false;
+        }
+    }
+    return true;
+}
 
 fn string_to_array(input: &str) -> Vec<Vec<char>> {
     let mut output: Vec<Vec<char>> = Vec::new();
     let mut row: Vec<char> = Vec::new();
     for (i, c) in input.chars().enumerate() {
-        if i % 3 == 0 && i != 0 {
+        if i % 9 == 0 && i != 0 {
             output.push(row);
             row = Vec::new();
         }
